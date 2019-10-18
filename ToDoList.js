4191a199a5fe753a222a9aad27d561e45891da67
Jon O'Drobinak <jodrobinak@gmail.com>
2019-10-17 23:22:46 -0600

    Add todolist class

---
+++ ToDoList.js
@@ -0,0 +1,12 @@
+class ToDoList {
+  constructor(id, title, urgent, tasks) {
+    this.id,
+    this.title,
+    this.urgent = urgent || false,
+    this.tasks = tasks || []
+  }
+
+  saveToStorage() {
+    
+  }
+}
